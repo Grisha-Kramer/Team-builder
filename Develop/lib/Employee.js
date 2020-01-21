a0723780e21032a5aa0459474b42ed7530ad9f0b
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-01-21 17:43:35 -0500

    7 tests passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,7 +1,21 @@
 class Employee{
-    constructor(name, id){
+    constructor(name, id, email, role){
         this.name = name;
         this.id = id;
+        this.email = email;
+        this.role = role;
     };
+    getName() {
+        return this.name
+    }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole() {
+        return this.role
+    }
 }
 module.exports = Employee
