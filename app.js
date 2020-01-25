4507bef086e27cd482cb4f345aa1f6508a19107f
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-01-25 15:45:39 -0500

    all individual cards working

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Engineer = require("./Develop/lib/Engineer");
 const open = require("open");
 const cards = require("./Develop/templates/cards");
 
-// function initialize() {
+
     return inquirer
     .prompt ([
         {
@@ -43,16 +43,14 @@ const cards = require("./Develop/templates/cards");
             }
             ]).then((inputs)=> {
                 let engineer = new Engineer(inputs.name, inputs.id, inputs.email, inputs.github);
-                console.log(engineer)
-
                 const head = cards.header()
-                console.log(head)
+                
                 const store = cards.engineer(engineer)
-                console.log(store)
+             
                 const foot = cards.footer()
-                console.log(foot)
+                
                 const concatenate = head + store + foot
-                fs.writeFile("team.html", concatenate, function (err) {
+                fs.writeFile("./Develop/output/team.html", concatenate, function (err) {
                     console.log(err)
                 })
 
@@ -86,9 +84,18 @@ const cards = require("./Develop/templates/cards");
                 name: "office"
             }
             ]).then((inputs)=>{
-                let manager = new manager(inputs.name, inputs.id, inputs.email, inputs.office)
-                Team.splice(team.length-1,0,manager.getHTML());
-                makeTeam();
+                let manager = new Manager(inputs.name, inputs.id, inputs.email, inputs.office)
+                
+                const head = cards.header()
+                
+                const store = cards.manager(manager)
+             
+                const foot = cards.footer()
+                
+                const concatenate = head + store + foot
+                fs.writeFile("./Develop/output/team.html", concatenate, function (err) {
+                    console.log(err)
+                })
             })
         }
         else if (replies.role === "Intern"){
@@ -115,24 +122,17 @@ const cards = require("./Develop/templates/cards");
             }
             ]).then((inputs)=>{
                 let intern = new Intern(inputs.name, inputs.id, inputs.email, inputs.school)
-                Team.splice(team.length-1,0,intern.getHTML());
-                makeTeam();
-            })
-    }
-    return printHTML(team);
-});
-    function printHTML(team){
-        fs.writeFile("team.html", team) 
 
-    open("team.html");
-    
-    
-
-
-initalize()
-.then((answers)=>{
-  const manager = new Manager(answers.name, answers.id, answers.email,answers.officeNumber);
-  team.splice(team.length-1,0,manager.getHTML());
-  buildTeam();
-})
-    }
+                const head = cards.header()
+                
+                const store = cards.intern(intern)
+             
+                const foot = cards.footer()
+                
+                const concatenate = head + store + foot
+                fs.writeFile("./Develop/output/team.html", concatenate, function (err) {
+                    console.log(err)
+                })
+            });
+        }
+    })
